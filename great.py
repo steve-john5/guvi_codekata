669d5e190e51625916f016fba343f512df4b025b
steve-john5 <36658855+steve-john5@users.noreply.github.com>
2018-05-17 10:17:33 +0530

    Create great.py

---
+++ great.py
@@ -0,0 +1,10 @@
+e=int(input("Enter the number1:"))
+f=int(input("Enter the number2:"))
+g=int(input("Enter the number3:"))
+if(e>f):
+  if(e>g):
+    print(e)
+elif(f>g):
+  print(f)
+else:
+  print(g)
